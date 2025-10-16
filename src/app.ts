43d90bc35470a33c00732bb6306fa59f721c923a
Ryan Savara <ryan.savara@vatusa.net>
2025-10-16 19:21:48 -0400

    error message fix

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,10 @@ import { vatusaSync } from './sync.js';
 
 if (!process.env['ZAU_API_URL'] || !process.env['ZAU_API_KEY'] || !process.env['VATUSA_API_KEY']) {
 	console.error(
-		'Missing at least one environment variable. Check to make sure the follow are set: "ZAU_API_URL", "ZAU_API_KEY',
-		'VATUSA_API_KEY',
+		'Missing at least one environment variable. Check to make sure the follow are set: "VATUSA_API_KEY", "ZAU_API_URL", "ZAU_API_KEY',
 	);
 	process.exit(4);
 }
-
 const task = new Cron('*/10 * * * *', () => vatusaSync());
 
 console.log(`Performing startup run. Next scheduled run is at: ${task.nextRun()}`);
