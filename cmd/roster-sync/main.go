b3b4a9779319fb3426ffc3f875bde381ce75981a
Ryan Savara <32147285+ryansavara@users.noreply.github.com>
2026-03-20 08:42:19 -0400

    catch trailing spaces in env vars

--- cmd/roster-sync/main.go
+++ cmd/roster-sync/main.go
@@ -17,8 +17,12 @@ import (
 	"github.com/vzauartcc/roster-sync/internal/zau"
 )
 
+func getEnvVariable(key string) string {
+	return strings.TrimSpace(os.Getenv(key))
+}
+
 func main() {
-	if os.Getenv("ZAU_API_URL") == "" || os.Getenv("ZAU_API_KEY") == "" || os.Getenv("VATUSA_API_KEY") == "" {
+	if getEnvVariable("ZAU_API_URL") == "" || getEnvVariable("ZAU_API_KEY") == "" || getEnvVariable("VATUSA_API_KEY") == "" {
 		panic("Missing at least one environment variable. Check to make sure the following are set: 'VATUSA_API_KEY', 'ZAU_API_KEY', 'ZAU_API_URL'.")
 	}
 
@@ -36,7 +40,7 @@ func main() {
 
 	runner.Start()
 
-	if os.Getenv("LOCAL_DEV_ENVIRONMENT") != "" {
+	if getEnvVariable("LOCAL_DEV_ENVIRONMENT") != "" {
 		log.Println("Invoked from script, running initial sync")
 
 		go doRosterSync(ctx)
